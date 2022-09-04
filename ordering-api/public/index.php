63a40e7a60f71a19b4f0b7bdcbb3fc003d923e70
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2022-09-04 12:57:19 +0200

    Create index file to prove proxypass working

---
+++ ordering-api/public/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Przekierowanie endpoint /api na kontener php działa poprawnie:";
+echo PHP_EOL;
+echo "<br/>";
+
+echo "Wynik nagłówka GET:";
+echo "<br/>";
+echo PHP_EOL;
+
+print_r($_GET);
