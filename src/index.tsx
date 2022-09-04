7638b3a3c3d1f39b7854abc7335179041e192573
BartoszKut <bartosz.kut.programista@gmail.com>
2022-09-04 12:45:11 +0200

    Remove unnecessary root element declaration

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import {StrictMode} from 'react';
-
 import { Xyz } from './components/xyz';
-
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
     const root = ReactDOM.createRoot(rootElement);
-    
+
     root.render(
         // Only for dev purposes
         <StrictMode>
