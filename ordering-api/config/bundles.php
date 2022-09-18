f1e67357db1edd416ccc1fc95143a3765cc1bc33
Wojciech Kluz <urban1996n@gmail.com>
2022-09-18 15:54:52 +0200

    Remove all mongodb references from API code

--- ordering-api/config/bundles.php
+++ ordering-api/config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
