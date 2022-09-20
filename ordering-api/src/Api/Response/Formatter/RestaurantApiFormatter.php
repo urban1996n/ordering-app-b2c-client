ab6753e75c3cfb2e8639699b904827e8c4a1776c
Wojciech Kluz <urban1996n@gmail.com>
2022-09-20 18:46:30 +0200

    Use new import namespace for RestaurantSettings

--- ordering-api/src/Api/Response/Formatter/RestaurantApiFormatter.php
+++ ordering-api/src/Api/Response/Formatter/RestaurantApiFormatter.php
@@ -4,7 +4,7 @@ namespace App\Api\Response\Formatter;
 
 use App\Entity\Settings\MenuSettings;
 use App\Entity\Restaurant;
-use App\Entity\RestaurantSettings;
+use App\Entity\Settings\RestaurantSettings;
 use App\Entity\Settings\Menu\MenuCategory;
 use App\Entity\Settings\Menu\MenuItem;
 
