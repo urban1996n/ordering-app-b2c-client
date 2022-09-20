6ae416ed735ccdc502589fb4e9cb9429eaf6c4b6
Wojciech Kluz <urban1996n@gmail.com>
2022-09-20 21:24:14 +0200

    Cleanup imports

--- ordering-api/src/Form/Type/Settings/Menu/MenuCategoryType.php
+++ ordering-api/src/Form/Type/Settings/Menu/MenuCategoryType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use FOS\RestBundle\Controller\Annotations\View;
 
 class MenuCategoryType extends AbstractType
 {
--- ordering-api/src/Kernel.php
+++ ordering-api/src/Kernel.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
