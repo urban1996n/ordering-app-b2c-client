1038bbac3ce66f861a1e14dda69af4df689f1e4f
Wojciech Kluz <urban1996n@gmail.com>
2022-09-20 22:35:08 +0200

    Add EventSubscriber to form

--- ordering-api/src/Form/Type/RestaurantType.php
+++ ordering-api/src/Form/Type/RestaurantType.php
@@ -3,6 +3,7 @@
 namespace App\Form\Type;
 
 use App\Entity\Restaurant;
+use App\Form\EventListener\RestaurantFormListener;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -10,11 +11,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RestaurantType extends AbstractType
 {
+    private RestaurantFormListener $restaurantFormListener;
+
+    /** @required */
+    public function setUpListener(RestaurantFormListener $restaurantFormListener): void
+    {
+        $this->restaurantFormListener = $restaurantFormListener;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name', TextType::class)
-            ->add('picture', TextType::class);
+            ->add('picture', TextType::class)
+            ->addEventSubscriber($this->restaurantFormListener);
     }
 
     public function configureOptions(OptionsResolver $resolver)
